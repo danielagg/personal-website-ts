7ca636fcea4df940c4056ecd5f57f00ae6fe3ba5
Daniel Agg <daniel.agg@outlook.com>
2019-08-24 18:25:42 +0200

    Wired in Typewriter

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./style.css";
+import TypeWriter from "../typewriter";
 
 const Landing = () => {
+  useEffect(() => {
+    initializeWriter();
+  }, []);
+
+  const initializeWriter = () => {
+    const textElement = document.querySelector(".typed-intro");
+
+    if (textElement != undefined) {
+      new TypeWriter(
+        textElement,
+        ["Hi, I'm Dan.", "I'm a full-stack dev."],
+        1500
+      );
+    }
+  };
+
   return (
     <div>
-      <p className="text-white">Hello.</p>
+      <h1 className="typed-intro relative text-xl inline-block font-raleway-bold text-white uppercase wide-letter-spacing"></h1>
+      <hr className="bg-white w-1/5 border-0 mt-2" style={{ height: "2px" }} />
+      <p>
+        full stack developer, employed by Glownexus SA this is a second line of
+        text to add, add and maybe a bit longer
+      </p>
     </div>
   );
 };
