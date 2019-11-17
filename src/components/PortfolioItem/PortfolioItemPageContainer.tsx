3dbf78efc44449999a55d5526273026e1cb028f3
Daniel Agg <daniel.agg@outlook.com>
2019-11-17 15:04:00 +0100

    Initial state alignments, view site/github links

--- src/components/PortfolioItem/PortfolioItemPageContainer.tsx
+++ src/components/PortfolioItem/PortfolioItemPageContainer.tsx
@@ -36,54 +36,92 @@ const PortfolioItemPageContainer = (props: PortfolioItemPageContainerProps) => {
     return (
       <div className="text-white lg:mt-8 mb-12">
         <ul>
-          {currentProject.keywords && currentProject.keywords.map((item, i) => <li className="inline-block text-xs lg:text-base mr-4 my-2 p-2 bg-purple-600 opacity-75" key={i}>#{item}</li> )}
+          {currentProject.keywords &&
+            currentProject.keywords.map((item, i) => (
+              <li
+                className="inline-block text-xs lg:text-base mr-4 my-2 p-2 bg-purple-600 opacity-75"
+                key={i}
+              >
+                #{item}
+              </li>
+            ))}
         </ul>
-        <h1 className="lg:text-5xl text-2xl font-raleway-bold mt-4 lg:mt-6">{currentProject.name}</h1>
-        <p className="lg:text-xl text-lg mt-4 lg:mt-8">{currentProject.description}</p>
+        <h1 className="lg:text-5xl text-2xl font-raleway-bold mt-4 lg:mt-6">
+          {currentProject.name}
+        </h1>
+        <p className="lg:text-xl text-lg mt-4 lg:mt-8">
+          {currentProject.description}
+        </p>
 
         <div className="flex flex-col lg:flex-row mt-8 lg:mt-12">
           <div className="lg:mr-3">
             <Button
-                  caption="visit site"
-                  url="#"
-                  variant={Variant.WhiteBorderOnly}
-                  thickness={Thickness.Widest}
-                />
+              caption="visit site"
+              isNewTab={true}
+              url={currentProject.githubLink}
+              variant={Variant.WhiteBorderOnly}
+              thickness={Thickness.Widest}
+            />
           </div>
-          
+
           <div className="mt-2 lg:mt-0">
             <Button
-                caption="view on GitHub"
-                url="#"
-                variant={Variant.White}
-                thickness={Thickness.Wider}
-              />
+              caption="view on GitHub"
+              isNewTab={true}
+              url={currentProject.githubLink}
+              variant={Variant.White}
+              thickness={Thickness.Wider}
+            />
           </div>
-          
         </div>
-        
-        <NavLink to="/" className="block mt-6 lg:mt-4 text-sm lg:text-base text-purple-400 hover:text-white">&larr; or go back to the homepage.</NavLink>
-        
+
+        <NavLink
+          to="/"
+          className="block mt-6 lg:mt-4 text-sm lg:text-base text-purple-400 hover:text-white"
+        >
+          &larr; or go back to the homepage.
+        </NavLink>
+
         <div className=" flex mt-8 lg:mt-16 flex-col-reverse lg:flex-row">
           <div className="pr-4 lg:pr-24">
             <p className="text-2xl mt-6 lg:mt-0">about the project</p>
-            {currentProject.detailedDescription && currentProject.detailedDescription.split('\n').map((item, i) => {
-              return <p key={i} className={`${i !== 0 ? "mt-4" : "mt-2"} opacity-75 leading-relaxed`}>{item}</p>;
-            })}
+            {currentProject.detailedDescription &&
+              currentProject.detailedDescription.split("\n").map((item, i) => {
+                return (
+                  <p
+                    key={i}
+                    className={`${
+                      i !== 0 ? "mt-4" : "mt-2"
+                    } opacity-75 leading-relaxed`}
+                  >
+                    {item}
+                  </p>
+                );
+              })}
           </div>
           <div className="lg:hidden flex-grow-0 flex-shrink-0">
             <p className="text-2xl">techs used</p>
             <ul className="list-disc mt-4">
-              {currentProject.technologies.map((item, i) => <li className="ml-4 p-1 opacity-75" key={i}>{item}</li> )}
+              {currentProject.technologies.map((item, i) => (
+                <li className="ml-4 p-1 opacity-75" key={i}>
+                  {item}
+                </li>
+              ))}
             </ul>
           </div>
           <div className="hidden lg:block lg:bg-white lg:p-10 text-primaryBlue font-raleway-bold flex-shrink-0 lg:w-1/3">
             <ul className="mt-4 mb-4">
-              {currentProject.technologies.map((item, i) => <li className={`${i !== 0 ? "mt-4" : "mt-0"} text-lg ml-4 p-1`} key={i}>{item}</li> )}
+              {currentProject.technologies.map((item, i) => (
+                <li
+                  className={`${i !== 0 ? "mt-4" : "mt-0"} text-lg ml-4 p-1`}
+                  key={i}
+                >
+                  {item}
+                </li>
+              ))}
             </ul>
           </div>
         </div>
-
       </div>
     );
   } else {
--- src/state/InitialState.ts
+++ src/state/InitialState.ts
@@ -44,7 +44,7 @@ export const InitialState: PortfolioState = {
       technologies: ["OData", "ASP.NET Core", "Entity Framework Core"],
       keywords: ["OData", "ASP.NET Core"],
       detailedDescription:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis nisi a eros suscipit lacinia. Suspendisse eget nibh nec nulla semper semper. Proin interdum eros et nibh vestibulum, id vulputate orci pharetra. Etiam dui erat, ultricies nec porttitor ut, pretium a orci. Maecenas sed pharetra dolor.\nSuspendisse varius suscipit malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean vitae arcu fermentum, finibus purus in, consectetur sem.\nPellentesque finibus vel orci quis varius. Aliquam sed metus risus. Cras auctor augue augue. Maecenas id lacus arcu. Donec lectus lacus, gravida a vulputate a, vehicula a massa. Duis commodo sapien eu nisl cursus, id porta tellus scelerisque. Aliquam ornare id sem non consequat. Suspendisse facilisis elit nec neque vestibulum convallis."
+        "I found that creating OData supported endpoints in an ASP.NET Core application can be challenging - one has to understand the convention based mechanisms, namings, services, etc.\nThis application is a small ASP.NET Core backend app with Entity Framework Core, which has a few OData endpoints to query playlists, albums, tracks and artists. It supports numerous standard OData features, such as $filter, $select, and has examples for handling complex objects and such. I tried to cover the use cases which I faced during my professional projects."
     },
     {
       id: 4,
@@ -53,13 +53,15 @@ export const InitialState: PortfolioState = {
       detailsLink: "personal-site",
       githubLink: "https://github.com/danielagg/personal-website-ts",
       technologies: [
-        "ReactJS (w/ Context API)",
+        "ReactJS",
+        "Context API",
+        "Hooks",
         "Tailwind CSS",
         "ZEIT integration"
       ],
-      keywords: [],
+      keywords: ["ReactJS", "ContextAPI", "Tailwind CSS"],
       detailedDescription:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis nisi a eros suscipit lacinia. Suspendisse eget nibh nec nulla semper semper. Proin interdum eros et nibh vestibulum, id vulputate orci pharetra. Etiam dui erat, ultricies nec porttitor ut, pretium a orci. Maecenas sed pharetra dolor.\nSuspendisse varius suscipit malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean vitae arcu fermentum, finibus purus in, consectetur sem.\nPellentesque finibus vel orci quis varius. Aliquam sed metus risus. Cras auctor augue augue. Maecenas id lacus arcu. Donec lectus lacus, gravida a vulputate a, vehicula a massa. Duis commodo sapien eu nisl cursus, id porta tellus scelerisque. Aliquam ornare id sem non consequat. Suspendisse facilisis elit nec neque vestibulum convallis."
+        "My ever-changing personal website: I use this project to play around with frontend techs.\nAs of right now, the site is using ReactJS with Typescript (create-react-app --typescript). TailwindCSS is used to design the components (CSS in JS). Since this is small project, I decided to skip using Redux (there is no real need to use middlewares, Sagas). Instead, the application uses React's Context API which actually provides quite a similar developer experience. There is no backend at the moment.\nThe site is deployed to my domain (danielagg.com) after each commit to the master branch, by using Zeit."
     },
     {
       id: 5,
