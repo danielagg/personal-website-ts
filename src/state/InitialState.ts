22c1ecb6e47667dff0b66b686c75cf6bb1d6c354
Daniel Agg <daniel.agg@outlook.com>
2020-01-28 13:40:51 +0100

    Summarizes - 1

--- src/state/InitialState.ts
+++ src/state/InitialState.ts
@@ -9,9 +9,9 @@ export const InitialState: PortfolioState = {
       appLink: "#",
       githubLink: "#",
       longDescription1:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis nisi a eros suscipit lacinia. Suspendisse eget nibh nec nulla semper semper. Proin interdum eros et nibh vestibulum, id vulputate orci ph.  amet, consectetur adipiscing elit. Duis mollis nisi a eros suscipit lacinia. Suspendisse",
+        "A full stack application, consisting of a ReactJS frontend (with Redux + Saga, Typescript, TailwindCSS), and an ASP.NET Core backend (EF Core, following Jason Taylor's clean architecture template). The application uses Google, Facebook and Twitter OAuth 2.0 account linking.",
       longDescription2:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis nisi a eros suscipit lacinia. Suspendisse eget nibh nec nulla semper semper. Proin interdum eros et nibh vestibulum, id vulputate orci pharetra. Etiam dui erat, ultricies nec porttitor ut, pre.",
+        "Guest users are able to view posts, however, only logged in users are able to create new ones - and of course, only edit/remove their own posts. The application supports basic pagination, as well as sorting, filtering and searching - for these requirements, OData was used.",
       shortDescription: "A platform to swap music equipments."
     },
     {
@@ -21,7 +21,7 @@ export const InitialState: PortfolioState = {
       appLink: "#",
       githubLink: "#",
       longDescription1:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis nisi a eros suscipit lacinia. Suspendisse eget nibh nec nulla semper semper. Proin interdum eros et nibh vestibulum, id vulputate orci pharetra. Etiam dui erat, ultricies nec porttitor ut, pretium a orci. Maecenas sed pharetra dolor. Suspendisse varius suscipit malesuada.",
+        "Another full stack application, this time, consisting of a VueJS frontend, and a NodeJS backend. In this project, I have used GraphQL with Apollo, and the same way of handling users via Google/Facebook/Twitter's OAuth 2.0.",
       longDescription2:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis nisi a eros suscipit lacinia. Suspendisse eget nibh nec nulla semper semper. Proin interdum eros et nibh vestibulum, id vulputate orci pharetra. Etiam dui erat, ultricies nec porttitor ut, pretium a orci. Maecenas sed pharetra dolor. Suspendisse varius suscipit malesuada. Vestibulum ante ipsum",
       shortDescription: "Run of the mill eCommerce app."
@@ -33,7 +33,7 @@ export const InitialState: PortfolioState = {
       appLink: "#",
       githubLink: "#",
       longDescription1:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis nisi a eros suscipit lacinia. Suspendisse eget nibh nec nulla semper semper. Proin interdum eros et nibh vestibulum, id vulputate orci pharetra. Etiam dui erat, ultricies nec porttitor ut, pretium a orci. Maecenas sed pharetra dolor. Suspendisse varius suscipit malesuada. ",
+        "This is another full stack application with a React frontend (uses the Context API instead of Redux), and ASP.NET Core backend. My goal with this app was to create a unique, modern UI that does not look like 'just another business application'.",
       longDescription2:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis nisi a eros suscipit lacinia. Suspendisse eget nibh nec nulla semper semper. Proin interdum eros et nibh vestibulum, id vulputate orci pharetra. Etiam dui erat, ultricies nec porttitor ut, pretium a orci. Maecenas sed pharetra dolor. Suspendisse varius suscipit malesuada. Vestibulum ante ipsum primis in.",
       shortDescription: "Made Up Contemporary Museum's site."
